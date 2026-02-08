993929001a5f8b75f87d6490ca48db26f9b7604b
dddddakari <dieynaba.sow@edu.sait.ca>
2026-02-07 23:08:35 -0700

    first changes
    This is just adding the proper imports and the basic stack formatting

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import React from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="about" options={{ title: 'About' }} />
+    </Stack>
+  );
 }
